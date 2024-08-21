5822cbfd4189c42156f6fee6da4ee9d51c7e03be
kearneyhe <kearneyhe@tencent.com>
2024-08-21 20:24:31 +0800

    2024.08.21

---
+++ src/main/java/com/example/leetcode/Hot100/leetcode/editor/cn/P5LongestPalindromicSubstring .java
@@ -0,0 +1,45 @@
+//给你一个字符串 s，找到 s 中最长的 回文 子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+//
+// Related Topics 双指针 字符串 动态规划 👍 7307 👎 0
+
+package com.example.leetcode.Hot100.leetcode.editor.cn;
+//java:最长回文子串
+class P5LongestPalindromicSubstring{
+    public static void main(String[] args){
+        Solution solution = new P5LongestPalindromicSubstring().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
